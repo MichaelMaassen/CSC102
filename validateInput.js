b9f76c39e5899d48950edb359d1536dc4ed7b682
Mike <105237840+MichaelMaassen@users.noreply.github.com>
2023-11-30 17:02:32 -0700

    Add files via upload

--- validateInput.js
+++ validateInput.js
@@ -1,38 +1,7 @@
 // This class is created to demonstrate inheritance
-class polygon {
-    constructor (sides) {
-        this.sides = sides;
-
-    }
-
-    alertSides(){
-        alert("this polygon has" + this.sides);
-    }
-
-    alertFormula(){
-        alert("The area of this object is")
-    }
-}
-
-class square extends polygon{
-    constructor (length){
-//Super refers to the parent of this child class
-        super(4);
-        this.length = length;
-    }
-
-    area(){
-        alert(this.length * this.length);
-    
-    }
-
-    alertFormula(){
-        super.alertFormula();
-        alert("...length squared")
-    }
-}
 
 function validateInput(){
+    console.log
     event.preventDefault();
     let firstName = document.getElementById("fname").value;
     console.log(firstName);
@@ -46,8 +15,8 @@ function validateInput(){
 
     console.log(firstLast.length);
 
-    if(firstLast.length < 20){
-        alert("Not enough characters. First and last name must have at least 20 characters.")
+    if(firstLast.length < 5){
+        alert("Not enough characters. First and last name must have at least 5 characters.")
         return;
     }
     console.log("First Last valid!");
@@ -65,13 +34,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if(form) {
        form.addEventListener("submit", validateInput); 
     }
-// This is refering to my polygon class. the polygon object is in var poly
-    var poly = new polygon(8);
-    poly.alertFormula();
-    poly.alertSides();
-
-    var squares = new square(3);
-//Calls alert formula for square and parent polygon (polymorphism)    
-    squares.alertFormula();
-    squares.area();
 });
